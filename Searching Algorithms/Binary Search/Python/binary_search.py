2451bbcfd2856e7ba2938da1449808711d82f3a4
yakann <mahmut_yakan44@hotmail.com>
2019-12-17 11:38:39 +0300

    Binary Search - python

---
+++ Searching Algorithms/Binary Search/Python/binary_search.py
@@ -0,0 +1,18 @@
+def binarySearch(seq,f):
+    left = 0
+    right = len(seq)-1
+    
+    while left <= right:
+        mid = round((left + right)/2)
+
+        if seq[mid] == f:
+            return seq[mid]
+        elif seq[mid] < f:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+seq = [1,2,5,6,7,8,9,44,46,71,79,88]
+
+print("Searched number: 88")
+print("Result: ",binarySearch(seq,88))
