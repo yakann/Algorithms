801c797b72dbe6b9fbf24cd63ed93059eacec4d8
yakann <mahmut_yakan44@hotmail.com>
2019-12-18 10:58:17 +0300

    linear search - python

---
+++ Searching Algorithms/Linear Search/Python/linear_search.py
@@ -0,0 +1,21 @@
+def linear_search(seq, f):
+
+    len_seq = len(seq)
+
+    for i in range(0, len_seq-1):
+
+        if seq[i] == f:
+            return "Founded value: "+ str(seq[i])
+
+    return "There is no such value."
+
+seq = [1,2,5,6,7,8,9,44,46,71,79,88]
+
+f = int(input("Value to search: "))
+
+print(linear_search(seq, f))
+
+"""
+Worst Case: O(n)
+
+"""
