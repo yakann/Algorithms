8918cf43ddabeda85be74fe6f3af880c68662ff7
yakann <mahmut_yakan44@hotmail.com>
2019-12-19 20:49:54 +0300

    merge sort-dont finished

---
+++ Sorting Algorithms/Merge Sort/Python/merge_sort.py
@@ -0,0 +1,24 @@
+
+def main_method(seq):
+
+
+    len_seq = len(seq)
+    while (round(len_seq/2) >= 1):
+    
+        left_merge = [] 
+    right_merge = []
+
+    default_seq = []
+
+    m_merge()
+
+def m_merge():
+    pass
+
+
+if __name__ == "__main__":
+
+    seq = [38,27,43,3,9,82,10]
+    
+
+    main_method(seq)
